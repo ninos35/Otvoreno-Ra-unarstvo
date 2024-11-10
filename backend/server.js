74a9a18f9e3bad2002e3ae35c531c4cae692fa60
ninos35 <nino.salai@fer.hr>
2024-11-11 00:44:49 +0100

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,48 @@
+const express = require("express")
+const app = express()
+const path = require("path")
 
+
+app.set("views",path.join(__dirname,"../frontend/public"))
+app.set("view engine","ejs")
+
+app.use(express.static(path.join(__dirname,"../frontend/public")))
+
+app.get("/",(req,res)=>{
+    console.log("here")
+    // res.send("hello")
+    //res.download("server.js")
+   // res.json({message:"error"})
+   res.render("index")
+})
+
+const {Client} = require("pg")
+
+const client = new Client({
+    host: "localhost",
+    user:"postgres",
+    port:5433,
+    password:"bazepodataka",
+    database:"Hoteli"
+})
+
+client.connect();
+
+
+
+app.get("/datatable",(req,res)=>{
+    
+    client.query("select h.id,ime_hotela,ulica,kucni_broj,postanski_broj,grad,drzava,broj_osoba,cijena,brojzvjezdica,telefon,email,s.id as soba_id from hotel h join sobe s on h.id=s.hotel_id",
+        (err,dbRes)=>{
+        if(!err){
+            res.render("datatable",{rooms:dbRes.rows})
+        }
+        else {
+            console.log(err.message)
+        }
+        client.end;
+    })
+
+})
+
+app.listen(3000)
