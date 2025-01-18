ff882ba48c76f9648a637aaa20373a0c48ca573f
ninos35 <nino.salai@fer.hr>
2025-01-18 22:04:23 +0100

    Add files via upload

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,63 +1,27 @@
+window.onload = function() {
 
- $(document).ready(function() {
-
-    let table = $('#myTable').DataTable({
-        responsive: true,
-        searching: true,
-        pageLength:25,
-        lengthChange:false,
-        paging:false,
-        info:false,
-        order: [[0, 'asc']] // prvi stupac ascending 
-        });
-
-});
-
-function myFunction(){
-    document.getElementById("mojDropdown").classList.toggle("show");
-}
-
-//zatvaranje dropdowna
-window.onclick = function(event){
-    if(!event.target.matches("dropbutton")){
-        var dropdowns = document.getElementsByClassName("dropcontent");
-        var i;
-        for(i=0;i<dropdowns.length;i++){
-            var openDropdown=dropdowns[i];
-            if(openDropdown.classList.contains("show")){
-                openDropdown.classList.remove("show")
-            }
+    fetch('/profile', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json', // Set headers if needed
+        },
+        credentials: 'same-origin'  // Include cookies for session-based authentication (if needed)
+      })
+      .then(response=>{
+        if(!response.ok){
+            document.getElementById("profilelink").style.display="none"
+            document.getElementById("logoutlink").style.display="none"
+            document.getElementById("refreshlink").style.display="none"
+            document.getElementById("loginlink").style.display="inline"
         }
-    }
-}
-
-$(document).ready(function() {
-    const table = $('#myTable').DataTable();
-
-    
-    $('#searchInput').on('keyup', function() {
-        const searchValue = this.value;
-        const filterType = $('#filterType').val();
-
-        
-        if (filterType === "hotel") {
-            table.columns(0).search(searchValue).draw(); // trazi po hotelima
-            table.columns(4).search(''); // ocisti filter za grad
-        } else if (filterType === "grad") {
-            table.columns(4).search(searchValue).draw(); 
-            table.columns(0).search(''); 
-        } else {
-            table.search(searchValue).draw();
+        else{
+            document.getElementById("profilelink").style.display="inline"
+            document.getElementById("logoutlink").style.display="inline"
+            document.getElementById("refreshlink").style.display="inline"
+            document.getElementById("loginlink").style.display="none"
+            
         }
-    });
-
+      })
     
-    $('#filterType').on('change', function() {
-        $('#searchInput').val(''); // ocisti tekst
-        table.search('').columns().search('').draw(); // resetiram za sve stupce
-    });
-});
-
-
-
-
+       
+}
